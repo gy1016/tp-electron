19df09a6b476bc04ea11ba191aa77d1629b80acc
lamb <1838940294@qq.com>
2023-02-13 20:04:07 +0800

    chore: 初始化webpack构建工具

---
+++ build/webpack.base.config.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const configuration: webpack.Configuration = {
+  entry: "./src/renderer/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "../dist"),
+    filename: "scripts/[name].[contenthash].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader",
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles/[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "../public", "index.html"),
+      filename: "index.html",
+      title: "TP-任务池",
+      inject: "body",
+      favicon: "./public/favicon.ico",
+    }),
+  ],
+};
+
+export default configuration;
---
+++ src/renderer/index.tsx
@@ -0,0 +1,3 @@
+const add = (x: number, y: number) => x + y;
+let z = add(1, 1);
+console.log(z);
