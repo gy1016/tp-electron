0ab080fb63787d4851808b73eefd0500bbb8345c
lamb <1838940294@qq.com>
2023-02-15 11:39:43 +0800

    feat: 在preload中添加IPC处理函数

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,30 +1,59 @@
-// import path from "path";
-import { app, BrowserWindow } from "electron";
+import path from "path";
+import { app, BrowserWindow, ipcMain } from "electron";
 import { resolveHtmlPath } from "./util";
 
-function createWindow() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    // webPreferences: {
-    //   preload: path.join(__dirname, "preload.js"),
-    // },
-  });
+let mainWindow: BrowserWindow | null = null;
 
-  win.loadURL(resolveHtmlPath("index.html"));
-}
-
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+ipcMain.on("ipc-example", async (event, arg) => {
+  const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
+  console.log(arg, msgTemplate(arg));
+  event.reply("ipc-example", msgTemplate("pong"));
 });
 
+const createWindow = async () => {
+  const RESOURCES_PATH = app.isPackaged
+    ? path.join(process.resourcesPath, "assets")
+    : path.join(__dirname, "../../assets");
+
+  const getAssetPath = (...paths: string[]): string => {
+    return path.join(RESOURCES_PATH, ...paths);
+  };
+
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 728,
+    icon: getAssetPath("icon.png"),
+    webPreferences: {
+      preload: app.isPackaged
+        ? path.join(__dirname, "preload.js")
+        : path.join(__dirname, "../../.erb/dll/preload.js"),
+    },
+  });
+
+  mainWindow.loadURL(resolveHtmlPath("index.html"));
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+};
+
+app
+  .whenReady()
+  .then(() => {
+    createWindow();
+    app.on("activate", () => {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (mainWindow === null) createWindow();
+    });
+  })
+  .catch(console.log);
+
 app.on("window-all-closed", () => {
+  // Respect the OSX convention of having the application in memory even after all windows have been closed
   if (process.platform !== "darwin") {
     app.quit();
   }
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,5 @@
+import "./App.css";
+
 const App = () => <div>sgy is handsome!</div>;
 
 export default App;
